234aa0288e387db22ccd9b9a5a7952dfa82b80f7
Steffen Linßen <>
2023-11-21 16:54:48 +0100

    minimized theme color selector

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
 
-	<div>MOIN</div>
-    <!-- <v-app theme="instaxTheme">
-        <v-main class="app">
-            <PrinterAppTemplate v-on:color-update="themeColor=$event" v-on:config-update="config=$event">
-                <template v-slot:default="{ printerStatus, print, color }">
-                    <router-view :color="color" :config="config" :printerStatus="printerStatus" :print="print" />
-                </template>
-            </PrinterAppTemplate>
-        </v-main>
-    </v-app> -->
+	<div class="app-area">
+		<div class="background" :style="themeStyling"/>
+		sdjklfm
+		<ThemeColorSelector v-on:color-change="themeColor = $event"/>
+	</div>
 </template>
 
 <script setup lang="ts">
-
+import ThemeColorSelector from '@/components/layout/ThemeColorSelector.vue'
 // import PrinterAppTemplate from '@/components/layout/PrinterAppTemplate.vue'
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const config=ref({
     width: 800,
@@ -24,75 +19,35 @@ const config=ref({
 
 const themeColor=ref<string>('pink');
 
+const themeStyling = computed(() => {
+	return {
+		backgroundColor: `var(--${themeColor.value}-color)`
+	}
+})
 
 </script>
 
-<style lang="scss">
-.app {
-    overflow: hidden !important;
-}
-</style>
 
-
-<style>
-@import url('https://fonts.googleapis.com/css?family=Inter');
-
-html,
-body {
-    overflow: hidden !important;
-    font-family: 'Inter', sans-serif;
-}
-</style>
 
 
 
 <style lang="scss">
-/* For Webkit, Blink, and Edge browsers */
-::-webkit-scrollbar {
-    width: 11px;
-    padding-right: 2px !important;
-    height: 14px;
-    background-color: transparent;
+.app-area {
+	width: 100%; 
+	height: 100vh; 
+	overflow: hidden;
 }
 
-::-webkit-scrollbar-track {
-
-    margin-top: 2px;
-    margin-bottom: 2px;
-    margin-right: 5px !important;
-    background-color: transparent;
+.background {
+	background-color: var(--green-color);
+	opacity: .1;
+	position: absolute;
+	top: 0px;
+	z-index: -1;
+	left: 0px;
+	height: 100%; 
+	width: 100%;
 }
 
-::-webkit-scrollbar-thumb {
-    background: #c0c0c0;
-    margin-top: 10px;
-    border-radius: 10px;
-    border: 1px solid #FFFFFF;
-}
 
-::-webkit-scrollbar-thumb:hover {
-    background: #a0a0a0;
-}
-
-/* For Mozilla Firefox */
-* {
-
-    margin-right: 5px;
-    scrollbar-width: thin;
-    scrollbar-color: #c0c0c0 transparent;
-}
-
-.hide-scrollbar {
-    overflow-y: scroll;
-    -ms-overflow-style: none !important;
-    /* IE and Edge */
-    scrollbar-width: none !important;
-    /* Firefox */
-}
-
-.hide-scrollbar::-webkit-scrollbar {
-    display: none !important;
-}
-
-/* Custom scrollbar styles for Internet Explorer are not supported */
 </style>
