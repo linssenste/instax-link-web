5a02e698e830def6e1a0cfd1cc75ffa11eb0e424
Steffen Linßen <>
2023-11-28 13:44:50 +0100

    fontawesome as default icons

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import "./assets/geist/stylesheet.css"
+import '@fortawesome/fontawesome-free/css/all.css';
 
 import "./style.css";
 createApp(App).mount('#app')
