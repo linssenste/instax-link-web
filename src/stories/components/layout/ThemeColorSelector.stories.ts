e99787452b4c34860d6be4cf48cfea24d33ea271
Steffen Linßen <linssenste@gmail.com>
2024-02-19 11:05:50 +0100

    added story + modified tests for theme color selection

--- src/stories/components/layout/ThemeColorSelector.stories.ts
+++ src/stories/components/layout/ThemeColorSelector.stories.ts
@@ -22,16 +22,25 @@ type Story = StoryObj<typeof meta>;
 
 
 /**
- * TBW
+ * This component sets the theme color for the whole application. The color is set as a CSS variable ```--dynamic-bg-color`` and thereby updates the theme of the app. It also emits the selected color as an event and also saves the selection in local storage to use this selection on the next reload. Most components of the app (buttons, selectors, text, etc.) dynamically change based on this selection. 
  */
- export const Default: Story = {
+ export const Event: Story = {
 	decorators: [
 	  () => ({
-		template: '<story v-on:type-change="action"/>',
-		methods: { action: action('type-change') }
+		template: '<story v-on:color-change="action"/>',
+		methods: { action: action('color-change') }
 	  }),
 	],
-
   };
+
+
+  export const Theme: Story = {
+	decorators: [
+	  () => ({
+		template: '<div style="position: relative; "><div style="position: absolute; background-color: var(--dynamic-bg-color); height: 100%; width: 100%; top: 0px; left: 0px; opacity: .5; z-index: -1; border-radius: 10px"/> <story style="padding: 20px" /></div>'
+	  }),
+	],
+  };
+
 
 
