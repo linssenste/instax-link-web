fd2544771a34ed06bcb343985fbb3ba019529e08
Steffen Linßen <linssenste@gmail.com>
2024-02-23 13:05:02 +0100

    added storybook accessibility a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,13 @@
 import type { StorybookConfig } from '@storybook/vue3-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-mdx-gfm'
+	'storybook-addon-vue-slots',
+	'@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/vue3-vite',
@@ -16,4 +17,6 @@ const config: StorybookConfig = {
     autodocs: 'tag'
   }
 }
+
+
 export default config
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,10 @@
 import type { Preview } from '@storybook/vue3'
 
+import "../src/assets/fonts/biro/stylesheet.css";
+import "../src/assets/fonts/keedy/stylesheet.css"
+import "../src/assets/fonts/rubik/stylesheet.css"
 import '../src/style.css';
+
 
 const preview: Preview = {
   parameters: {
