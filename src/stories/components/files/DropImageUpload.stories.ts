bf1b56f884dbb294b43f2bde5591c2b7b6f259f4
Steffen Linßen <linssenste@gmail.com>
2024-03-06 10:09:51 +0100

    added more unit tests and modified story

--- src/stories/components/files/DropImageUpload.stories.ts
+++ src/stories/components/files/DropImageUpload.stories.ts
@@ -4,13 +4,17 @@ import { action } from '@storybook/addon-actions'
 import DropImageUpload from '../../../components/files/DropImageUpload.vue';
 
 /**
- * TBD
+ * This component provides the drag-and-drop functionality for uploading image files. 
+ * This interactive component provides visual feedback when a user drags files over it, 
+ * supporting a user-friendly file upload experience. The component emits an event with 
+ * the dropped file when a user drops an image file onto it, allowing for further 
+ * handling of the uploaded file.
  */
 const meta = {
-  title: 'Files/Drag & Drop',
-  component: DropImageUpload,
+	title: 'Files/Drag & Drop',
+	component: DropImageUpload,
 
-  tags: ['autodocs'],
+	tags: ['autodocs'],
 } satisfies Meta<typeof DropImageUpload>;
 
 export default meta;
@@ -20,16 +24,16 @@ type Story = StoryObj<typeof meta>;
 
 
 
- export const Upload: Story = {
+export const Upload: Story = {
 	decorators: [
-	  () => ({
-		template: '<div style="width: 300px; height: 300px; position: relative;  border-radius: 10px; overflow: hidden"><story v-on:dropped="action"/></div>',
-		methods: { action: action('dropped') }
-	  }),
+		() => ({
+			template: '<div style="width: 300px; height: 400px; position: relative;  border-radius: 10px; overflow: hidden"><story v-on:dropped="action"/></div>',
+			methods: { action: action('dropped') }
+		}),
 	],
 
 
-  };
+};
 
 
 
