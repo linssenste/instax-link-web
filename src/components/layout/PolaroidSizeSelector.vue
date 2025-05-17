3d9300ccb5ef03ebc5093e6cba4c10e486e50162
linssenste <linssenste>
2025-05-17 18:00:23 +0200

    added random image animation on selected polaroid + type as text

--- src/components/layout/PolaroidSizeSelector.vue
+++ src/components/layout/PolaroidSizeSelector.vue
@@ -1,30 +1,52 @@
 <template>
-	<div oncontextmenu="return false" class="size-selector">
+	<div>
+		<div oncontextmenu="return false" class="size-selector">
+			<div v-for="filmType in [InstaxFilmVariant.MINI, InstaxFilmVariant.SQUARE, InstaxFilmVariant.WIDE]"
+				:key="filmType" :title="polaroidTitle(filmType)" :style="polaroidClass(filmType)"
+				@click="selectedType = filmType" class="polaroid" :data-testid="`polaroid-selector-${filmType}`">
 
-		<div v-for="filmType in [InstaxFilmVariant.MINI, InstaxFilmVariant.SQUARE, InstaxFilmVariant.LARGE]" :key="filmType"
-			 :title="polaroidTitle(filmType)" :style="polaroidClass(filmType)" @click="selectedType = filmType"
-			 class="polaroid" :data-testid="`polaroid-selector-${filmType}`">
-			<div class="inner-polaroid"></div>
+				<!-- inner polaroid develops (fade-in) and shows random image if selected -->
+				<div class="inner-polaroid">
+					<img v-show="selectedType === filmType" v-on:load="beginImageDevelopment"
+						referrerpolicy="no-referrer" crossorigin="anonymous"
+						v-on:error="setFallbackImage($event, filmType)" :src="randomPolaroidImage(filmType)"
+						:data-testid="`image-${filmType}`" alt="random miniature image" width="100%" height="100%" />
+
+					<div class="overlay" :id="`${filmType}-overlay`"></div>
+				</div>
+			</div>
+
 		</div>
 
+
+		<div class="info-text">
+			TYPE: <b>{{ selectedType.toUpperCase() }}</b>
+		</div>
 	</div>
 </template>
-  
+
 <script setup lang="ts">
 import { ref, watch } from 'vue';
 import { InstaxFilmVariant } from '../../interfaces/PrinterStateConfig';
 
-const selectedType = ref<InstaxFilmVariant>(InstaxFilmVariant.SQUARE);
+const selectedType = ref<InstaxFilmVariant>(localStorage.getItem("polaroid") ?? InstaxFilmVariant.SQUARE);
 
 // events
 const emit = defineEmits<{
 
 	/**
-	 * emits new selected type (mini, square or large)
+	 * emits new selected type (mini, square or wide)
 	 * @param {InstaxFilmVariant} type new polaroid size type
 	 */
 	(e: 'type-change', type: InstaxFilmVariant): void;
 }>();
+
+
+// emit change event
+watch(selectedType, () => {
+	emit('type-change', selectedType.value);
+	localStorage.setItem("polaroid", selectedType.value);
+}, { immediate: true });
 
 
 // hovering html title
@@ -32,24 +54,66 @@ const polaroidTitle = (filmType: InstaxFilmVariant) => {
 	switch (filmType) {
 		case InstaxFilmVariant.MINI: return "Instax Mini (600x800)"
 		case InstaxFilmVariant.SQUARE: return "Instax Square (800x800)"
-		case InstaxFilmVariant.LARGE: return "Instax Large (1260x840)"
+		case InstaxFilmVariant.WIDE: return "Instax Wide (1260x840)"
 		default: break;
 	}
+}
+
+const imageHeight = 40
+
+const imageWidth = (filmType: InstaxFilmVariant) => {
+	return (filmType == InstaxFilmVariant.MINI ? (3 / 4) : (filmType == InstaxFilmVariant.SQUARE ? 1 : (3 / 2))) * imageHeight;
 }
 
 // styling
 const polaroidClass = (filmType: InstaxFilmVariant) => {
 	return {
-		backgroundColor: selectedType.value === filmType ? 'var(--dynamic-bg-color)' : 'var(--grey-color)',
-		width: `${filmType == InstaxFilmVariant.MINI ? 18 : (filmType == InstaxFilmVariant.SQUARE ? 25 : 40)}px`,
+		width: `${imageWidth(filmType)}px`,
 		boxShadow: `0px 0px 5px rgba(0, 0, 0, ${filmType === selectedType.value ? .25 : 0})`,
+		transform: filmType === selectedType.value ? 'scale(1.15)' : "",
 	}
 }
 
-// emit change event
-watch(selectedType, () => {
-	emit('type-change', selectedType.value);
+
+const retryCount = ref(0); // retry count for image loading
+
+// set fallback image (dog jasper) if image loading fails
+function setFallbackImage(e: Event, filmType: InstaxFilmVariant) {
+	if (retryCount.value > 5 || filmType !== selectedType.value) return;
+
+	const target = e.target as HTMLImageElement;
+	retryCount.value++;
+
+	if (!target) return;
+	target.src = `/public/fallback-images/fallback-${selectedType.value.charAt(0)}.webp`;
+
+}
+
+// start image development on selected polaroid after image loaded (fade-out overlay)
+function beginImageDevelopment() {
+	console.log("begin image development");
+	const overlay = document.getElementById(`${selectedType.value}-overlay`) as HTMLElement;
+	console.log(overlay);
+	if (overlay) {
+		overlay.classList.add('develop-polaroid');
+	}
+}
+
+// generate random image for selected polaroid
+function randomPolaroidImage(filmType: InstaxFilmVariant) {
+	return filmType == selectedType.value ? `https://picsum.photos/seed/${Math.random().toString(36).slice(2)}/${imageWidth(filmType) * 2}/${imageHeight * 2}` : '';
+}
+
+// remove overlay on previous polaroid
+watch(selectedType, (newType, oldType) => {
+	const overlay = document.getElementById(`${oldType}-overlay`) as HTMLElement;
+	if (overlay) {
+		overlay.classList.remove('develop-polaroid');
+	}
 }, { immediate: true });
+
+
+
 
 </script>
 
@@ -57,23 +121,46 @@ watch(selectedType, () => {
 .polaroid {
 	position: relative;
 	padding: 3px;
-	height: 35px;
+	height: 48px;
+	padding-top: 4px;
 	border-radius: 2px;
 	cursor: pointer;
-	transition: all 150ms;
+	background-color: var(--white-color);
+	transition: transform 250ms;
+	;
 }
 
 .inner-polaroid {
-	background-color: var(--light-grey-color) !important;
+	position: relative;
 	width: 100%;
-	height: 28px;
+	height: 40px;
 	background-size: cover;
 	background-position: center;
 	border-radius: 0;
+	border-radius: 1px;
+	background-color: var(--grey-color);
+	overflow: hidden;
+}
+
+.inner-polaroid>.overlay {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: var(--black-color) !important;
+	opacity: 1;
+}
+
+
+.inner-polaroid>.overlay.develop-polaroid {
+
+	transition: opacity 1s;
+	opacity: 0;
 }
 
 .polaroid:hover {
-	transform: scale(1.02);
+	transform: scale(1.05);
 	box-shadow: 0px 0px 5px rgba(0, 0, 0, .1)
 }
 
@@ -82,8 +169,31 @@ watch(selectedType, () => {
 	position: relative;
 	display: flex;
 	flex-direction: row;
-	gap: 8px;
-	width: 130px;
+	gap: 10px;
 	justify-content: center;
+}
+
+
+.info-text {
+	position: relative;
+	margin-top: 12px;
+	width: 100%;
+	font-size: 15px;
+	color: var(--black-color);
+}
+
+
+@media (max-width: 1000px) {
+	.size-selector {
+
+		transform: scale(1.25);
+	}
+
+	.info-text {
+		margin-top: 25px;
+		font-size: 17px;
+
+
+	}
 }
 </style>
